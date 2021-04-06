b495461d35423a3f619387649f8e365cc58c8087
park hyunbin <wise7231@gmail.com>
2021-04-06 18:09:25 +0900

    DTO, Validation

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,8 +12,8 @@ export class MoviesService {
         return this.movies;
     }
     
-    getOne(id : string): Movie {
-        const movie = this.movies.find(movie => movie.id === parseInt(id));
+    getOne(id : number): Movie {
+        const movie = this.movies.find(movie => movie.id === id);
         if (!movie) {
             throw new NotFoundException(`Movie with Id ${id} not found`);
         }
@@ -19,20 +21,20 @@ export class MoviesService {
         return movie;
     }
 
-    deleteOne(id: string): boolean {
+    deleteOne(id: number): boolean {
         this.getOne(id);
-        this.movies = this.movies.filter(movie => movie.id !== +id);
+        this.movies = this.movies.filter(movie => movie.id !== id);
         return true;
     }
 
-    create(movieData) {
+    create(movieData: CreateMovieDto) {
         this.movies.push({
             id: this.movies.length + 1,
             ...movieData,
         });
     }
     
-    update(id: string, updateData) {
+    update(id: number, updateData: UpdateMovieDto) {
         const movie = this.getOne(id);
         this.deleteOne(id);
         this.movies.push({ ...movie, ...updateData});        
